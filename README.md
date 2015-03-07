f1f2b5d3e2d49f714ef07bce064744ae23af2a23
Xin Wang <sutarshow@gmail.com>
2015-03-07 19:53:21 +0100

    Packaging project for PyPI

--- README.md
+++ README.md
@@ -4,27 +4,45 @@ JSON2Mantle
 Generate [Mantle](https://github.com/Mantle/Mantle) models using JSON files.
 
 ##Quick start
+
+### Install 
+
+```
+pip3 install JSON2Mantle
+```
+
 ### Usage
 
-`$ python3 JSON2Mantle.py [-h] [--prefix PREFIX] json_file output_dir`
+```
+json2mantle [-h] [--prefix PREFIX] [--author AUTHOR]
+                   json_file output_dir
+
+```
 
 ### Example
-`$ python3 JSON2Mantle.py api_model.json class --prefix XYZ`
 
-will generate Mantle models according to your `api_model.json` structure. The output files will be created under `output_dir` directory, and the Objective-C classes have the prefix `XYZ`.
+```
+json2mantle api_model.json class --prefix XYZ --author "John Smith"
+```
+
+will generate Mantle models according to your `api_model.json` structure. The output files will be created under `output_dir` directory, the author name will be `John Smith`, and the Objective-C classes have the prefix `XYZ`.
 
 ## Features
+
 * Supports nested JSON data, which means JSON2Mantle can generate the correct number of classes that the JSON file contains.
 * Convert field name like `var_name` to `varName` automatically.
-* Written in Python 3.
+* Python 3.
 
-## Notice
-* `setup` is under construction.
+## Note
+
+* When reserved words in Objective-C appear, it will replace the original name with a prefix `model`. For instance, if you have a field with the name `id`, the generated one would be `modelId`.
 * Only supports generating Objective-C files.
+* If the input JSON file is an array, it will ask you to give a name for the array items.
 
 ## TODO
 - [ ] reserved words
 - [ ] URL type
+- [ ] Python 2 compatible
 
 ## License
 The MIT License (MIT)
--- setup.py
+++ setup.py
@@ -2,32 +2,41 @@ from setuptools import setup, find_packages
 import sys
 import os
 
-version = '0.0.2'
+version = '0.0.3'
 
-with open('README.md') as f:
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
 
-
-setup(name='JSON2Mantle',
-      version=version,
-      description='Generate Mantle models using JSON files',
-      long_description=readme,
-      author='Xin Wang',
-      author_email='i@wangx.in',
-      url='https://github.com/sutar/JSON2Mantle',
-      license=license,
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points={
-          "console_scripts": [
-              "json2mantle = json2mantle.cli:main",
-          ]
-      },
-      )
+setup(
+    name='JSON2Mantle',
+    version=version,
+    description='Generate Mantle models using JSON files',
+    long_description=readme,
+    author='Xin Wang',
+    author_email='i@wangx.in',
+    url='https://github.com/sutar/JSON2Mantle',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Objective C',
+        'Topic :: Text Processing',
+    ],
+    keywords='mantle parser objective-c',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      # -*- Extra requirements: -*-
+    ],
+    entry_points={
+      "console_scripts": [
+          "json2mantle = json2mantle.cli:main",
+      ]
+    },
+)
